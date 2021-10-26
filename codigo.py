a471f6ec023d59a3e850ff254c3a18392f1badb9
Giovana Carmazio <83242981+Giovanacarmazio@users.noreply.github.com>
2021-10-26 14:01:10 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -4,5 +4,22 @@ fila_1 = random.sample(range(1, 30), 20)
 
 fila_2 = random.sample(range(1, 30), 20)
 
-print("Fila 1: {}".format(fila_1))
-print("Fila 2: {}".format(fila_2))
+numerocres1=sorted(fila_1)
+numerocres2=sorted(fila_2) 
+
+print(numerocres1)
+print(numerocres2)
+
+fila_um = random.sample(range(1, 30), 20)
+fila_dois = random.sample(range(1, 30), 20)
+
+fila_um.sort(reverse=False)
+fila_dois.sort(reverse=False)
+
+sobrepoe = [i for i in set(fila_um) if i in fila_dois]
+
+print('Fila um:', fila_um)
+
+print('Fila dois:', fila_dois)
+
+print('Numeros repetidos:', sobrepoe)
